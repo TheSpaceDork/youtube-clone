e248187981692b3769510542c55ccd716fcee638
TheSpaceDork <treasurechux@gmail.com>
2023-01-10 17:35:48 +0100

    push clone initially

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "681bb1dc0fmsh1ae6402f3a706dbp1a0b8bjsn2c12b1c102b3",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
